1f2535e856d26c8e0a051eb927e4db13f41beaae
jsriveraa1 <js.riveraa1@uniandes.edu.co>
2022-11-29 17:40:40 -0500

    #### Internacionalización app. * Se ajustan los textos de la aplicación, se define los idiomas _español_ e _ingles_ como idiomas predeterminados.

--- app/src/main/java/com/example/vinilos/repostories/ArtistRepository.kt
+++ app/src/main/java/com/example/vinilos/repostories/ArtistRepository.kt
@@ -2,16 +2,16 @@ package com.example.vinilos.repostories
 
 import android.app.Application
 import android.util.Log
-import com.example.vinilos.model.*
+import com.example.vinilos.model.AlbumCreate
+import com.example.vinilos.model.Artist
+import com.example.vinilos.model.ArtistDetail
 import com.example.vinilos.services.ArtistService
-import com.example.vinilos.services.CacheManager
-import com.example.vinilos.services.CollectoService
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import org.json.JSONArray
 import kotlin.coroutines.resume
 import kotlin.coroutines.resumeWithException
 import kotlin.coroutines.suspendCoroutine
-import kotlinx.serialization.json.Json
-import kotlinx.serialization.decodeFromString
-import org.json.JSONArray
 
 class ArtistRepository(private val application: Application) {
 
